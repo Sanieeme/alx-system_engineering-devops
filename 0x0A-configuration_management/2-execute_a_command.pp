44ecb2586e44e9dd20f39f87c77c0c832833417e
Sanieeme <shoneesani@gmail.com>
2024-03-22 05:16:31 -0700

    command

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,7 +1,4 @@
-#!/usr/bin/pup
 # create a manifest that kills a process named killmenow
 exec { 'killmenow':
   command     => 'pkill killmenow',
-  path        => '/bin:/usr/bin:/sbin:/usr/sbin',
-  refreshonly => true,
 }  
